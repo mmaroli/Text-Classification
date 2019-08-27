301cfd8165ca75e40bd956cc177d37992686848b
Manav Maroli <manav.maroli@leafgroup.com>
2019-08-27 13:58:24 -0700

    Reduce GPU memory usage

--- classifiers/neural_classifier.py
+++ classifiers/neural_classifier.py
@@ -15,6 +15,16 @@ class Neural:
         self.model_path = f'./models/tensorflow_{self.label_level}.model'
         self.embedding_model = 'https://tfhub.dev/google/tf2-preview/nnlm-en-dim128/1'
         self.data_loader = NeuralLoader(level=self.label_level)
+        # Reduce GPU Memory Usage
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                print(e)
 
     def prepare_training_data(self):
         """ Prepares training, validation, and test data in parquet format """
